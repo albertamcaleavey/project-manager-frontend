2d53ef1ae6485555689de1d6ef8b64769c0a7aa2
Alberta McAleavey <a.mcaleavey24@gmail.com>
2022-04-13 17:24:44 -0400

    fix date formatting on task cards

--- src/pages/ProjectDetails/components/TaskCard.jsx
+++ src/pages/ProjectDetails/components/TaskCard.jsx
@@ -2,13 +2,11 @@ import './TaskCard.css'
 import moment from 'moment';
 
 const TaskCard = ({task}) => {
-  // let currentDate = new Date()
 
   return (  
     <div className='task-card'>
-      <h6>{moment(task?.date).format("MMM Do YY")}</h6>
+      <h6>{moment(task?.date).add(1, 'days').format("MMM Do YY").toString()}</h6>
       <p>{task?.description}</p>
-      {/* <p>{task?.complete ? 'true' : 'false'}</p> */}
     </div>
   );
 }
